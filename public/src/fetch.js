bf10063b654b7f5048b12213939eb7a208d2293f
cgloveut <pitsanubomb@gmail.com>
2022-01-25 21:13:43 +0700

    :fire: Hot FIX :bug: add device id to multiple case

--- public/src/fetch.js
+++ public/src/fetch.js
@@ -35,6 +35,25 @@ async function postAuth(url = '', data = {}) {
     return response.json(); // parses JSON response into native JavaScript objects
 }
 
+async function pathAuth(url = '') {
+    // Default options are marked with *
+    const token = localStorage.getItem('auth')
+    const response = await fetch(url, {
+        method: 'PATCH', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token,
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        // body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
 async function getAuth(url = '') {
     // Default options are marked with *
     const token = localStorage.getItem('auth')
